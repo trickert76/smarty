52b5c070efcc6256f3ef0b5fb1eb768fdc05dcd6
Thoralf Rickert-Wendt <thoralf@m84.de>
2026-01-30 12:58:22 +0100

    fix: cleaned up documentation

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 cd /srv/smarty
 
-# aufräumen
+# clean up
 rm -rf var/www/backup.tar.gz
 
-# Templates kopieren für die Seite
-cp bin/*               var/www/dist/.
-cp etc/cron            var/www/dist/.
-cp docker-compose.yml  var/www/dist/.
-cp .env                var/www/dist/env
-cp var/www/index.php   var/www/dist/index.php.html
-
-# Sicherung erstellen
+# create backup
 tar -czf /tmp/backup.tar.gz .
 mv /tmp/backup.tar.gz var/www/backup.tar.gz
 
+#eof
